9c217bfb82147f573a90a97181cf484eb2f14dae
Aleksey <alekseyvasiljev@gmail.com>
2018-11-28 18:13:15 +0300

    Second lesson 1.0

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -18,19 +18,18 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
             Iterable<Message> messages = messageRepo.findAll();
             model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
         messageRepo.save(message);
@@ -39,8 +38,24 @@ public class GreetingController {
 
     @PostMapping("filter")
     public String filter(@RequestParam String filter, Map<String, Object> model) {
-        List<Message> messages = messageRepo.findByTag(filter);
+        Iterable<Message> messages;
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
         model.put("messages", messages);
         return "main";
+    }
+
+    @PostMapping("removeById")
+    public String removeById(@RequestParam Integer numForDel, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if(numForDel == null) {
+            return "main";
+        } else {
+            messageRepo.deleteById(numForDel);
+            return "redirect:main";
+        }
     }
 }
